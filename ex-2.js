4a1bfaf1fe9d33e1c97369511df0f8ed7107d408
Eye <zab_poosa@hotmail.com>
2024-04-07 00:08:19 +0700

    function-ex

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,20 @@
 // Start coding here
+let add = function (a, b) {
+    return a + b;
+}
+console.log(add(10,5));
+
+let subtract = function (a, b) {
+    return a - b;
+}
+console.log(subtract(10,5));
+
+let multiply = function (a, b) {
+    return a * b;
+}
+console.log(multiply(10,5));
+
+let divide = function (a, b) {
+    return a / b;
+}
+console.log(divide(10,5));
--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,12 @@
 // Start coding here
+let add = (a, b) => a + b;
+console.log(add(10,5));
+
+let subtract = (a, b) => a - b;
+console.log(subtract(10,5));
+
+let multiply = (a, b) => a * b;
+console.log(multiply(10,5));
+
+let divide = (a, b) => a / b;
+console.log(divide(10,5));
